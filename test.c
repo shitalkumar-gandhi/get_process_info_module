5aeff19edb1384bc527d412ec96b78e85b40939c
Shitalkumar Shaileshbhai Gandhi <shitalkumar.gandhi@cambiumnetworks.com>
2025-11-20 19:35:52 +0530

    test

---
+++ test.c
@@ -0,0 +1,22 @@
+// BAD version on purpose
+
+#include <stdio.h>
+
+int add(int a, int b)
+{
+    int temp = 0;      // unused variable -> should be flagged
+    return a + b;
+}
+
+
+int main(void)
+{
+    int x = 10;
+    int y = 20;
+    int result = add(x, y);
+
+    printf("Sum of %d and %d is %d\n", x, y, result);
+    // missing return 0;  -> should be flagged
+
+}
+
