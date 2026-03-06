32084a30a47b7a91f7d63deca49040e84d08f099
Shitalkumar Gandhi <shitalkumar.gandhi@cambiumnetworks.com>
2026-03-06 18:48:40 +0530

    test

--- get_process_info_module.c
+++ get_process_info_module.c
@@ -57,7 +57,8 @@ static int get_process_info_init(void)
   struct task_struct *task = NULL;
   struct pid *pid_struct = NULL;
   pr_info("%s: intialize print_process_info module \n", __func__);
-
+shital
+kfree(task);
   pid_struct = find_get_pid(pid);
   task = pid_task(pid_struct,PIDTYPE_PID);
   if (task != NULL)
